98317b69f864c8411137aeec007b3ab4d6a9a1c0
“tiagoluis12” <tiago7164@hotmail.com>
2024-06-20 15:13:05 +0100

    config jest/ tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,27 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/test/(.+)': '<rootDir>/test/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  testMatch: ['**/*.spec.ts'],
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/test'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  },
+  // clearMocks: true,
+  // setupFiles: ['dotenv/config']
+}
+
+module.exports = config;
