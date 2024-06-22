e09068abce96dde8cd8cd245cf2c4327b5348474
“tiagoluis12” <tiago7164@hotmail.com>
2024-06-22 18:42:57 +0100

    sql

--- create.sql
+++ create.sql
@@ -1,13 +1,16 @@
-drop schema taxi_app cascade
 
-create schema taxi_app
+drop schema taxi_app cascade;
 
-create table taxi_app.account (
-  account_id_uuid primary key,
-  name text not null,
-  email text not null,
-  cpf text not null,
-  car_plate text null,
-  is_passenger boolean not null default false,
-  is_driver boolean not null default false,
-)
+
+create schema taxi_app;
+
+
+create table taxi_app.account
+( account_id uuid primary key,
+ name text not null,
+ email text not null,
+ cpf text not null,
+ car_plate text null,
+ is_passenger boolean not null default false,
+ is_driver boolean not null default false);
+
